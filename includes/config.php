9705e8cc368cf0aa55d487438e2b96ae4fe39cf9
Lau-tech1 <82028963+Lau-tech1@users.noreply.github.com>
2024-09-12 15:24:49 +0800

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,5 +1,5 @@
 <?php
-  define( 'DB_HOST', 'database-1.cza86zaqydzg.us-east-1.rds.amazonaws.com' );          // Set database host
+  define( 'DB_HOST', 'webdb.c6rifb3ewyy4.us-east-1.rds.amazonaws.com' );          // Set database host
   define( 'DB_USER', 'admin' );             // Set database user
   define( 'DB_PASS', 'lab-password' );             // Set database password
   define( 'DB_NAME', 'sample' );        // Set database name

21336354bf78b731148abb3e2d9273adf407552d
Lau-tech1 <82028963+Lau-tech1@users.noreply.github.com>
2024-09-12 15:22:50 +0800

    Add files via upload

---
+++ includes/config.php
@@ -0,0 +1,7 @@
+<?php
+  define( 'DB_HOST', 'database-1.cza86zaqydzg.us-east-1.rds.amazonaws.com' );          // Set database host
+  define( 'DB_USER', 'admin' );             // Set database user
+  define( 'DB_PASS', 'lab-password' );             // Set database password
+  define( 'DB_NAME', 'sample' );        // Set database name
+
+?>
